92fd7ea5dce0788b3765c4d4a8c0082589111baf
AC <workingbb9@gmail.com>
2026-02-13 10:33:34 -0700

    feat: add parsing for path of http request

--- src/handler.rs
+++ src/handler.rs
@@ -2,23 +2,63 @@ use crate::parser::{Message, Protocol};
 
 pub fn handle_client(msg: Message) -> String {
     match msg.protocol {
-        Protocol::Http => handle_http(msg.content),
+        Protocol::Http => handle_http(&msg.content),
         Protocol::Unknown => handle_unknown(),
     }
 }
 
-fn handle_http(req: String) -> String {
-    let reply = format!("Hello from Polaris\nYour request: {}", req);
-    let response = format!(
-        "HTTP/1.1 200 OK\r\n\
-        Content-Length: {len}\r\n\
-        Content-Type: text/plain\r\n\
-        Connection: close\r\n\
-        \r\n\
-        {reply}",
-        len = reply.len(),
-        reply = reply
-    );
+fn handle_http(req: &str) -> String {
+    let page = match req {
+        "/" =>
+            Some(format!(
+                r#"
+                <!DOCTYPE html>
+                <html>
+                    <head><title>Polaris</title></head>
+                    <body>
+                        <h1>Hello from Polaris<h1>
+                        <h2>You said: {req}<h2>
+                    </body>
+                </html>
+                "#,
+                req = req
+            )),
+        "/about" =>
+            Some(format!(
+                r#"
+                <!DOCTYPE html>
+                <html>
+                    <head><title>Polaris</title></head>
+                    <body>
+                        <h1>About Polaris<h1>
+                        <h2>A general purpose web-server<h2>
+                    </body>
+                </html>
+                "#,
+            )),
+        _ => None,
+    };
+        
+    let response = match page {
+        Some(s) =>
+            format!(
+                "HTTP/1.1 200 OK\r\n\
+                Content-Length: {len}\r\n\
+                Content-Type: text/html\r\n\
+                Connection: close\r\n\
+                \r\n\
+                {reply}",
+                len = s.len(),
+                reply = s
+            ),
+        None =>
+            "HTTP/1.1 404 Not Found\r\n
+            Content-Length: 13\r\n
+            Content-Type: text/plain\r\n
+            Connection: close\r\n
+            \r\n
+            404 Not Found".to_string(),
+    };
 
     response
 }
--- src/parser.rs
+++ src/parser.rs
@@ -9,16 +9,38 @@ pub struct Message {
 }
 
 pub fn parse_msg(msg: &str) -> Message {
-    let content = msg.lines().next().unwrap_or("");
-    let protocol = match content {
-        s if s.starts_with("GET ") => Protocol::Http,
-        s if s.starts_with("POST ") => Protocol::Http,
-        s if s.starts_with("HTTP") => Protocol::Http,
+    let first_line = msg.lines().next().unwrap_or("");
+    let mut content: &str = "";
+
+    let protocol = match first_line {
+        s if s.starts_with("GET ") => {
+            content = parse_http(first_line);
+            Protocol::Http
+        }
+        s if s.starts_with("POST ") => {
+            content = parse_http(first_line);
+            Protocol::Http
+        }
+        s if s.starts_with("HTTP") => {
+            content = parse_http(first_line);
+            Protocol::Http
+        }
         _ => Protocol::Unknown,
     };
+    
 
     Message {
         protocol,
         content: content.to_string(),
     }
+}
+
+fn parse_http(first_line: &str) -> &str {
+    let mut parts = first_line.split_whitespace();
+    
+    let _method = parts.next();
+    let path = parts.next();
+    let _proto = parts.next();
+
+    path.unwrap_or("")
 }

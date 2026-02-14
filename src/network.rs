67e2d9249200fd155e24b759f7d654a73eb37820
AC <workingbb9@gmail.com>
2026-02-14 12:58:17 -0700

    feat: add timeout to free up memory

--- src/network.rs
+++ src/network.rs
@@ -1,15 +1,19 @@
 use tokio::io::Result;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
+use tokio::time::{Duration, timeout};
 
 pub async fn get_msg(stream: &mut TcpStream) -> Result<Option<Vec<u8>>> {
     let mut buf = [0u8; 1024];
 
-    if stream.read(&mut buf).await? == 0 {
-        return Ok(None);
-    }
+    let result = timeout(Duration::from_secs(5), stream.read(&mut buf)).await;
 
-    Ok(Some(buf.to_vec()))
+    match result {
+        Ok(Ok(0)) => Ok(None),
+        Ok(Ok(_)) => Ok(Some(buf.to_vec())),
+        Ok(Err(e)) => Err(e),
+        Err(_) => Ok(None),
+    }
 }
 
 pub async fn send_msg(msg: &[u8], stream: &mut TcpStream) -> Result<()> {

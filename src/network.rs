96872f758588219019cfd6f5e55f52f0f745435e
AC <workingbb9@gmail.com>
2026-03-03 21:14:04 -0700

    feat: add linear buffer for reading stream

--- src/network.rs
+++ src/network.rs
@@ -3,16 +3,30 @@ use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
 use tokio::time::{Duration, timeout};
 
+const BUF_SIZE: usize = 8192;
+
 pub async fn get_msg(stream: &mut TcpStream) -> Result<Option<Vec<u8>>> {
-    let mut buf = [0u8; 1024];
+    let mut buf = Vec::with_capacity(BUF_SIZE);
+    let mut tmp = [0u8; 1024];
 
-    let result = timeout(Duration::from_secs(5), stream.read(&mut buf)).await;
+    loop {
+        let result = timeout(Duration::from_secs(5), stream.read(&mut tmp)).await;
 
-    match result {
-        Ok(Ok(0)) => Ok(None),
-        Ok(Ok(_)) => Ok(Some(buf.to_vec())),
-        Ok(Err(e)) => Err(e),
-        Err(_) => Ok(None),
+        match result {
+            Ok(Ok(0)) => return Ok(None),
+            Ok(Ok(n)) => {
+                buf.extend_from_slice(&tmp[..n]);
+                if find_header_end(&buf).is_some() {
+                    return Ok(Some(buf));
+                }
+
+                if buf.len() > BUF_SIZE {
+                    return Err(std::io::Error::other("Header too large"));
+                }
+            }
+            Ok(Err(e)) => return Err(e),
+            Err(_) => return Ok(None),
+        };
     }
 }
 
@@ -20,4 +34,8 @@ pub async fn send_msg(msg: &[u8], stream: &mut TcpStream) -> Result<()> {
     stream.write_all(msg).await?;
     stream.flush().await?;
     Ok(())
+}
+
+fn find_header_end(buf: &[u8]) -> Option<usize> {
+    buf.windows(4).position(|w| w == b"\r\n\r\n").map(|i| i + 4)
 }

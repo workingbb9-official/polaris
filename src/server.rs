5c2014971c6c7bc2ea925e885595c3c3fd972d5a
AC <workingbb9@gmail.com>
2026-03-17 12:40:07 -0600

    chore: add docs and comments to server module

--- src/server.rs
+++ src/server.rs
@@ -22,12 +22,14 @@ static DEFAULT_HTML_NOT_FOUND: &str = r#"<!DOCTYPE html>
 const BUF_SIZE: usize = 8192;
 const TIMEOUT_LEN: u64 = 5;
 
+/// Set default as simple html page.
 fn default_err_handler(_: &[u8]) -> HttpResponse {
     let body = DEFAULT_HTML_NOT_FOUND.as_bytes().to_vec();
 
     HttpResponse::new(body, "text/html".to_string())
 }
 
+/// Used to map a path to an action and response.
 pub struct Router {
     routes: HashMap<Vec<u8>, Handler>,
     err_handler: Handler,
@@ -45,10 +47,12 @@ impl Router {
         self.routes.insert(path.into(), handler);
     }
 
+    /// Set error handler for path not found.
     pub fn add_err_handler(&mut self, handler: Handler) {
         self.err_handler = handler;
     }
 
+    /// Map a path to given handler.
     pub fn handle(&self, path: &[u8]) -> HttpResponse {
         match self.routes.get(path) {
             Some(handler) => handler(path),
@@ -81,6 +85,7 @@ impl<P: Protocol + std::marker::Sync + std::marker::Send + 'static> Server<P> {
         })
     }
 
+    /// Connect to client and spawn a task.
     pub async fn run(self: Arc<Self>) -> tokio::io::Result<()> {
         loop {
             let (stream, _) = self.listener.accept().await?;
@@ -93,10 +98,15 @@ impl<P: Protocol + std::marker::Sync + std::marker::Send + 'static> Server<P> {
         }
     }
 
+    /// Handle a connection task for a single client.
+    ///
+    /// Receive bytes, parse, handle, format and send response.
+    ///
     async fn handle_connection(&self, mut stream: TcpStream) {
         let mut buf = network::SlidingBuffer::new(BUF_SIZE);
 
         loop {
+            // Receive from socket
             let pos = match network::get_msg(&mut stream, &mut buf, TIMEOUT_LEN).await {
                 Err(network::RecvError::HeaderTooLarge) => {
                     info!("Header too large");
@@ -113,6 +123,7 @@ impl<P: Protocol + std::marker::Sync + std::marker::Send + 'static> Server<P> {
                 Ok(n) => n,
             };
 
+            // Parse received data
             let p_msg = match self.protocol.parse(&buf.data()[..pos]) {
                 Some(p) => p,
                 None => {
@@ -127,13 +138,16 @@ impl<P: Protocol + std::marker::Sync + std::marker::Send + 'static> Server<P> {
                 }
             };
 
+            // Lookup handler for path and format response
             let resp = self.router.handle(&p_msg);
             let f_resp = self.protocol.format(&resp);
 
+            // Send message
             if let Err(e) = network::send_msg(&f_resp, &mut stream).await {
                 warn!("Failed to send msg with error: {}", e);
             }
 
+            // Move leftovers from next request to start of buffer
             buf.shift_leftovers(pos);
         }
     }

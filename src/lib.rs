19123030bc5b5cbc6a45a57a15117cdbdae8e779
AC <workingbb9@gmail.com>
2026-05-03 19:33:15 -0500

    refactor: use modern module structure

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,6 @@ mod transport;
 
 pub use controller::Controller;
 pub use device::{Device, DeviceId, DeviceType};
-pub use node::{Node, NodeApp};
+pub use node::{Node, NodeApp, NodeError, NodeEvent};
 pub use protocol::DataMessage;
 pub use transport::Transport;

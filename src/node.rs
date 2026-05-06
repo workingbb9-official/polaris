1a71774cc0af1140796a3d70b7c2c7110fe3c53a
AC <workingbb9@gmail.com>
2026-05-05 19:07:05 -0500

    refactor: remove transport trait from node

--- src/node.rs
+++ src/node.rs
@@ -23,7 +23,6 @@ use heartbeat::HeartbeatManager;
 
 use crate::device::Device;
 use crate::protocol::{DataMessage, DiscoveryMessage, HeartbeatMessage};
-use crate::transport::Transport;
 
 /// The core interface for [Node] logic.
 pub trait NodeApp {
@@ -45,16 +44,19 @@ pub enum NodeEvent {
     DataReceived,
 }
 
+#[derive(Debug)]
+pub enum NodeAction<Addr> {
+    SendHeartbeat { addr: Addr, msg: [u8; 3] },
+    SendDiscovery { msg: [u8; 3] },
+}
+
 /// The errors that can occur within a [Node].
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum NodeError<TE> {
+pub enum NodeError {
     /// The node is not connected to a controller, which is necessary for the operation.
     NotConnected,
-    /// There was an error sending or receiving over the transport. This holds the specific error
-    /// from the [Transport] trait.
-    Transport(TE),
-    /// The message received was invalid. This could be returned if the bytes could not be parsed
-    /// into a message object, or the message type was invalid for the current state.
+    /// The message sent was invalid. This could be due to an incorrect packet format that could
+    /// not be parsed, or the message type was invalid for the current state of the node.
     InvalidMessage,
     /// The message received was sent from somebody that was not the connected controller. The
     /// message is dropped for security.
@@ -66,30 +68,22 @@ pub enum NodeError<TE> {
 /// Nodes will remain simple and able to represent any type of device. They can be anything that
 /// operates independently and sends information to a controller.
 #[derive(Debug)]
-pub struct Node<T: Transport, A: NodeApp> {
+pub struct Node<Addr, App: NodeApp> {
     dev: Device,
-    controller: Option<T::Addr>,
+    controller: Option<Addr>,
     discovery: DiscoveryManager,
     heartbeat: HeartbeatManager,
-    transport: T,
-    app: A,
+    app: App,
 }
 
-impl<T: Transport, A: NodeApp> Node<T, A> {
+impl<Addr: Copy + std::cmp::PartialEq, App: NodeApp> Node<Addr, App> {
     /// Create a new node.
-    pub fn new(
-        dev: Device,
-        discovery_interval: u32,
-        heartbeat_interval: u32,
-        transport: T,
-        app: A,
-    ) -> Self {
+    pub fn new(dev: Device, discovery_interval: u32, heartbeat_interval: u32, app: App) -> Self {
         Self {
             dev,
             controller: None,
             discovery: DiscoveryManager::new(discovery_interval),
             heartbeat: HeartbeatManager::new(heartbeat_interval),
-            transport,
             app,
         }
     }
@@ -100,63 +94,31 @@ impl<T: Transport, A: NodeApp> Node<T, A> {
         self.dev
     }
 
-    /// Receive and process incoming packets, then send heartbeat or discovery as needed.
+    pub fn action(&mut self, now: u32) -> Option<NodeAction<Addr>> {
+        if let Some(addr) = self.controller {
+            if self.heartbeat.action(now) == HeartbeatAction::Send {
+                let msg = HeartbeatMessage::new(self.dev.id());
+                let raw = msg.to_bytes();
+                Some(NodeAction::SendHeartbeat { addr, msg: raw })
+            } else {
+                None
+            }
+        } else if self.discovery.action(now) == DiscoveryAction::Broadcast {
+            let msg = DiscoveryMessage::new_hello(self.dev.id());
+            let raw = msg.to_bytes();
+            Some(NodeAction::SendDiscovery { msg: raw })
+        } else {
+            None
+        }
+    }
+
+    /// Process a packet and return event.
     ///
     /// # Errors
     /// * `Err(NodeError::WrongController)` - Received a packet from a different address.
     /// * `Err(NodeError::InvalidMessage)` - The message received was invalid. This could be due to
     ///   an incorrect format, or a wrong packet type for the current Node state.
-    /// * `Err(NodeError::Transport(e))` - There was an error sending or receiving a packet.
-    pub fn process(&mut self, now: u32) -> Result<Option<NodeEvent>, NodeError<T::Error>> {
-        let event = self.receive()?;
-
-        if let Some(addr) = self.controller {
-            if self.heartbeat.action(now) == HeartbeatAction::Send {
-                self.send_heartbeat(&addr)?;
-            }
-        } else if self.discovery.action(now) == DiscoveryAction::Broadcast {
-            self.broadcast()?;
-        }
-
-        Ok(event)
-    }
-
-    /// Send a [DataMessage] to the controller.
-    ///
-    /// # Errors
-    /// * `Err(NodeError::NotConnected)` - There is no controller to send data to.
-    /// * `Err(NodeError::Transport(e))` - There was an error sending the data over the wire.
-    pub fn send_data(&mut self, msg: DataMessage) -> Result<(), NodeError<T::Error>> {
-        let Some(controller) = self.controller.as_ref() else {
-            return Err(NodeError::NotConnected);
-        };
-
-        let raw = msg.to_bytes();
-
-        self.transport
-            .send(&raw, controller)
-            .map_err(NodeError::Transport)
-    }
-
-    fn receive(&mut self) -> Result<Option<NodeEvent>, NodeError<T::Error>> {
-        let mut buf = [0u8; 260];
-        let (n, addr) = match self.transport.recv(&mut buf) {
-            Ok((n, addr)) => (n, addr),
-            Err(e) => return Err(NodeError::Transport(e)),
-        };
-
-        if n == 0 {
-            return Ok(None);
-        }
-
-        self.handle_msg(&buf[..n], addr)
-    }
-
-    fn handle_msg(
-        &mut self,
-        buf: &[u8],
-        addr: T::Addr,
-    ) -> Result<Option<NodeEvent>, NodeError<T::Error>> {
+    pub fn process_msg(&mut self, buf: &[u8], addr: Addr) -> Result<Option<NodeEvent>, NodeError> {
         if let Some(con_addr) = self.controller.as_ref() {
             if addr != *con_addr {
                 return Err(NodeError::WrongController);
@@ -179,104 +141,5 @@ impl<T: Transport, A: NodeApp> Node<T, A> {
 
             Ok(Some(NodeEvent::ControllerConnected))
         }
-    }
-
-    fn broadcast(&mut self) -> Result<(), NodeError<T::Error>> {
-        let msg = DiscoveryMessage::new_hello(self.dev.id());
-        let raw = msg.to_bytes();
-        let addr = self.transport.broadcast_addr();
-
-        self.transport
-            .send(&raw, &addr)
-            .map_err(NodeError::Transport)
-    }
-
-    fn send_heartbeat(&mut self, addr: &T::Addr) -> Result<(), NodeError<T::Error>> {
-        let msg = HeartbeatMessage::new(self.dev.id());
-        let raw = msg.to_bytes();
-
-        self.transport
-            .send(&raw, addr)
-            .map_err(NodeError::Transport)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::device::{DeviceId, DeviceType};
-
-    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-    struct MockAddr {
-        pub octets: [u8; 4],
-        pub port: u16,
-    }
-
-    #[derive(Debug, PartialEq, Eq)]
-    struct MockTransport;
-
-    impl Transport for MockTransport {
-        type Addr = MockAddr;
-
-        type Error = bool;
-
-        fn broadcast_addr(&mut self) -> Self::Addr {
-            new_mock_addr()
-        }
-
-        fn send(&mut self, _buf: &[u8], _addr: &Self::Addr) -> Result<(), Self::Error> {
-            Ok(())
-        }
-
-        fn recv(&mut self, buf: &mut [u8]) -> Result<(usize, Self::Addr), Self::Error> {
-            let addr = new_mock_addr();
-            Ok((buf.len(), addr))
-        }
-    }
-
-    #[derive(Debug, PartialEq, Eq)]
-    struct MockApp {
-        data: [u8; 260],
-    }
-
-    impl NodeApp for MockApp {
-        fn on_data(&mut self, data: &[u8]) {
-            let len = data.len().min(260);
-            self.data[..len].copy_from_slice(&data[..len]);
-        }
-
-        fn on_connection(&mut self) {}
-    }
-
-    fn new_mock_addr() -> MockAddr {
-        MockAddr {
-            octets: [127, 0, 0, 1],
-            port: 8080,
-        }
-    }
-
-    fn new_mock_node() -> Node<MockTransport, MockApp> {
-        let app = MockApp { data: [0u8; 260] };
-        let dev = Device::new(DeviceId::new(10), DeviceType::new(0));
-        let mut node = Node::new(dev, 100, 50, MockTransport, app);
-
-        let msg = DiscoveryMessage::new_welcome();
-        let raw = msg.to_bytes();
-        let addr = new_mock_addr();
-
-        node.handle_msg(&raw, addr).unwrap();
-        node
-    }
-
-    #[test]
-    fn test_handle_data() {
-        let mut node = new_mock_node();
-        let msg = DataMessage::new(DeviceId::new(11), b"turn_on");
-        let raw = msg.to_bytes();
-        let addr = new_mock_addr();
-
-        let ret = node.handle_msg(&raw, addr);
-        assert_eq!(ret, Ok(Some(NodeEvent::DataReceived)));
-        assert_eq!(node.app.data[..7], b"turn_on".to_vec());
     }
 }

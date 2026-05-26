3d0ca573455ba3a0d103bae01d1bc6b817db375d
AC <workingbb9@gmail.com>
2026-05-26 16:18:45 -0500

    chore: shorten licenses

--- src/device.rs
+++ src/device.rs
@@ -1,18 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
 // Copyright (c) 2026 workingbb9-official
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-#![allow(dead_code)]
 
 use zerocopy::{FromBytes, Immutable, IntoBytes, KnownLayout};
 
@@ -24,7 +11,7 @@ pub struct Device {
 }
 
 impl Device {
-    pub(crate) fn new(id: DeviceId, dev_type: DeviceType) -> Self {
+    pub fn new(id: DeviceId, dev_type: DeviceType) -> Self {
         Self { id, dev_type }
     }
 
@@ -48,7 +35,7 @@ pub struct DeviceId(u16);
 
 impl DeviceId {
     #[inline]
-    pub(crate) fn new(val: u16) -> Self {
+    pub fn new(val: u16) -> Self {
         Self(val)
     }
 
@@ -66,7 +53,7 @@ pub struct DeviceType(u16);
 
 impl DeviceType {
     #[inline]
-    pub(crate) fn new(val: u16) -> Self {
+    pub fn new(val: u16) -> Self {
         Self(val)
     }
 

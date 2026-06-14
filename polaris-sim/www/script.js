ed7d7ff051896814f343e8887e34c0efc5e897ae
AC <workingbb9@gmail.com>
2026-06-14 11:21:52 -0500

    feat(web): create and drop circle

--- polaris-sim/www/script.js
+++ polaris-sim/www/script.js
@@ -1,6 +1,7 @@
 import init, { Simulation } from "../pkg/polaris_sim.js";
 
 let sim = null;
+let circle = null;
 
 async function run() {
     await init();
@@ -17,10 +18,64 @@ function initEventListeners() {
     });
 
     const spawn = document.getElementById("spawn");
-    spawn.addEventListener("click", () => {
-        sim.spawn_node();
-        console.log("Node spawned");
-    });
+    spawn.addEventListener("click", createCircle);
+}
+
+function createCircle() {
+    if (circle) {
+        return;
+    }
+
+    circle = document.createElement("div");
+    circle.classList.add("circle");
+    document.body.appendChild(circle);
+
+    window.addEventListener("mousemove", moveCircle);
+    window.addEventListener("click", dropCircle);
+    window.addEventListener("keydown", cancelCircle);
+}
+
+function moveCircle(e) {
+    if (!circle) {
+        return;
+    }
+
+    circle.style.left = `${e.clientX}px`;
+    circle.style.top = `${e.clientY}px`;
+}
+
+function dropCircle(e) {
+    if (!circle) {
+        return;
+    }
+
+    if (e.target.tagName == "DIV" || e.target.tagName == "BUTTON") {
+        return;
+    }
+
+    circle.style.backgroundColor = "#cc5500";
+    window.removeEventListener("mousemove", moveCircle);
+    window.removeEventListener("click", dropCircle);
+    window.removeEventListener("keydown", cancelCircle);
+
+    console.log("Node spawned");
+    sim.spawn_node();
+    circle = null;
+}
+
+function cancelCircle(e) {
+    if (!circle) {
+        return;
+    }
+
+    if (e.key == "Escape") {
+        circle.remove();
+        window.removeEventListener("mousemove", moveCircle);
+        window.removeEventListener("click", dropCircle);
+        window.removeEventListener("keydown", cancelCircle);
+
+        circle = null;
+    }
 }
 
 run();
